ff1a43f478bb7a8471245e5bcd255100d6799e48
St33zy <stevenparks909@gmail.com>
2023-12-14 14:38:22 -0800

    First Commit for this assignment

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Block-7-Workshop-Secure-the-Vault
+This is my submission for this weeks assignment.
---
+++ script.js
@@ -0,0 +1,18 @@
+// Create a website popup alert that notifies the user to open vault
+// create 3 variables that equal to the lock combination utilizing different arithmetic operators
+// create some sort of pop up so that the lock combination appears
+
+window.alert("You have received this message because you have been chosen to open an important vault. Click the box below to find the super secret combination!!!!");
+
+num1 = 5 * 2 ;
+num2 = 80 / 2;
+num3 = 20 + 19;
+
+// console.log() was just messing around with things here
+
+function printMessage() {
+    let text = document.getElementById("message").innerHTML = `${num1} - ${num2} - ${num3}`;
+}
+
+
+
